ceb51855c44ed9107e3ce5cf43ee25000dceaa54
rowkaxl <rowkaxl@naver.com>
2021-03-29 17:00:00 +0900

    SwiftUI Fundamentals PART 1: SWIFTUI VIEWS

--- My SwiftUI/ContentView.swift
+++ My SwiftUI/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image(systemName: "swift")
+            Text("Hello, world!")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            
+        }
     }
 }
