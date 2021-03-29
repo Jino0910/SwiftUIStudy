bc4fea86b64c0e2b5f20ba9444154d629b1dd479
rowkaxl <rowkaxl@naver.com>
2021-03-29 15:27:06 +0900

    initalize

---
+++ My SwiftUI/My_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_SwiftUIApp.swift
+//  My SwiftUI
+//
+//  Created by rowkaxl on 2021/03/29.
+//
+
+import SwiftUI
+
+@main
+struct My_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
