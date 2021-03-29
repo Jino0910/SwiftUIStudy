b4ad7f20e04ec76cb551bad45d0dbf80065b99eb
rowkaxl <rowkaxl@naver.com>
2021-03-29 17:48:21 +0900

    Modifiers

--- My SwiftUI/ContentView.swift
+++ My SwiftUI/ContentView.swift
@@ -11,7 +11,14 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "swift")
+                .resizable()
+                .frame(width: 100, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .center)
+                .background(Color.orange)
+                .padding([.leading, .bottom, .trailing])
             Text("Hello, world!")
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .kerning(5.0)
+                .padding()
         }
     }
 }
