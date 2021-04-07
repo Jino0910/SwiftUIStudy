e44eafd107431b215be882ab820751b11fbf01ed
rowkaxl <rowkaxl@naver.com>
2021-04-07 14:09:13 +0900

    no message

---
+++ My SwiftUI/StateAndBinding.swift
@@ -0,0 +1,20 @@
+//
+//  StateAndBinding.swift
+//  My SwiftUI
+//
+//  Created by rowkaxl on 2021/04/07.
+//
+
+import SwiftUI
+
+struct StateAndBinding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StateAndBinding_Previews: PreviewProvider {
+    static var previews: some View {
+        StateAndBinding()
+    }
+}
