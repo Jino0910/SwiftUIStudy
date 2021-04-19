f77e8b2052fcea3e551f155fd8dfe9937ba4315c
rowkaxl <rowkaxl@naver.com>
2021-04-19 19:03:49 +0900

    StateAndBinding

--- My SwiftUI/StateAndBinding.swift
+++ My SwiftUI/StateAndBinding.swift
@@ -8,13 +8,42 @@
 import SwiftUI
 
 struct StateAndBinding: View {
+    @State private var swiftyColor: Color = .red
+    @State private var swiftyOpacity: Double = 0.7
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            SwiftyControls(swiftyColor: $swiftyColor,
+                           swiftyOpacity: $swiftyOpacity)
+            
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .padding(25)
+                .foregroundColor(.white)
+                .opacity(swiftyOpacity)
+                .background(swiftyColor)
+                .cornerRadius(50)
+        }
+        .padding()
     }
 }
 
 struct StateAndBinding_Previews: PreviewProvider {
     static var previews: some View {
         StateAndBinding()
+    }
+}
+
+struct SwiftyControls: View {
+    @Binding var swiftyColor: Color
+    @Binding var swiftyOpacity: Double
+    
+    var body: some View {
+        ColorPicker("Swifty Color",
+                    selection: $swiftyColor)
+        Slider(value: $swiftyOpacity,
+               in: 0...1)
+            .accentColor(swiftyColor)
     }
 }
